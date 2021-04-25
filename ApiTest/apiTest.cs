6e41e7cf2ff6fb18b2cad3246f3851c32e08a469
mokh179 <mohammed.khaled17997@yahoo.com>
2021-04-25 14:11:37 +0200

    test2

--- ApiTest/apiTest.cs
+++ ApiTest/apiTest.cs
@@ -10,17 +10,6 @@ namespace ApiTest
     
     public class apiTest
     {
-        IOperations<user> _db;
-        public apiTest(IOperations<user> db)
-        {
-            _db = db;
-        }
-        //apiCon _db = new apiCon();
-        [Fact]
-        public void Test1()
-        {
-            user u=_db.getByID(1);
-            Assert.Equal(1, u.ID);
-        }
+      
     }
 }
